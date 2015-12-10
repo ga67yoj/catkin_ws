37fb7b22802bbb3106367887ff5410704e841ebe
ga67yoj <christian.czempinski@tum.de>
2015-12-10 15:42:31 +0100

    init git usw2

---
+++ build/tas_car/simple_navigation_goals/CMakeFiles/send_goals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/send_goals.dir/src/send_goals.cpp.o"
+  "/home/tas/catkin_ws/devel/lib/simple_navigation_goals/send_goals.pdb"
+  "/home/tas/catkin_ws/devel/lib/simple_navigation_goals/send_goals"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/send_goals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
