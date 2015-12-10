66cb21b4776ebfe9ca73b3bb65a9bf6c11a83452
ga67yoj <christian.czempinski@tum.de>
2015-12-10 16:19:08 +0100

    init9001

--- build/tas_car/simple_navigation_goals/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tas/catkin_ws/src/tas_car/simple_navigation_goals
-# Build directory: /home/tas/catkin_ws/build/tas_car/simple_navigation_goals
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tas_car/tas/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tas/catkin_ws/src/tas_car/tas
-# Build directory: /home/tas/catkin_ws/build/tas_car/tas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
